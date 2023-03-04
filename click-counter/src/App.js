7dd25fe717bc98588259908f9fcd072498a413a4
OanhBui <oanhbp3105@gmail.com>
2023-03-04 00:07:29 -0800

    update App.js file

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -2,9 +2,14 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function App() {
+  const [num, setNum] = useState(0);
+  const handleClick = (e) => {
+    setNum((oldNum) => oldNum + 1);
+  };
   return (
     <div className="App">
-      
+      <h1>{num}</h1>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
